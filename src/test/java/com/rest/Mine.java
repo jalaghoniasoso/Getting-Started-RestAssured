b37878238ada8f2a5f43c782d91e596def45b122
jalaghoniasoso <jalaghoniasoso@gmail.com>
2025-01-04 01:21:16 +0400

    add

--- src/test/java/com/rest/Mine.java
+++ src/test/java/com/rest/Mine.java
@@ -10,18 +10,17 @@ import static io.restassured.RestAssured.given;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.*;
 
-public class AutomateGet {
+public class Mine {
     @org.testng.annotations.Test
     public void test() {
         given().
                 baseUri("https://api.postman.com").
                 header("x-api-key", "PMAK-6776d09287e3f900011928f6-6ea05caccf7d38082ae295b37d5faee465").
-                config(config.logConfig(LogConfig.logConfig().enableLoggingOfRequestAndResponseIfValidationFails())).
-                log().ifValidationFails().
+                log().all().
         when().
                 get("/workspaces").
         then().
-                log().ifValidationFails().
+                log().body().
                 assertThat().
                 statusCode(200);
     }
