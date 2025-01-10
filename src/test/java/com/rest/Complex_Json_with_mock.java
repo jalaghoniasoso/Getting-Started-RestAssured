c14a7511e19b5d2e9c0cb6b3b65a6feeaac8d5ab
jalaghoniasoso <67267624+jalaghoniasoso@users.noreply.github.com>
2025-01-10 13:03:05 +0400

    Update Complex_Json_with_mock.java

--- src/test/java/com/rest/Complex_Json_with_mock.java
+++ src/test/java/com/rest/Complex_Json_with_mock.java
@@ -22,7 +22,7 @@ public class Complex_Json_with_mock {
     public void beforeClass(){
         RequestSpecBuilder requestSpecBuilder = new RequestSpecBuilder().
                 setBaseUri("https://c4d02499-47b8-4b86-8625-2961ccf82030.mock.pstmn.io").
-                addHeader("x-api-key", "PMAK-6776d09287e3f900011928f6-6ea05caccf7d38082ae295b37d5faee465").
+                addHeader("x-api-key", "").
                 setContentType("application/json;charset=utf-8");
 //                setConfig(config.encoderConfig(EncoderConfig.encoderConfig()
 //                        .appendDefaultContentCharsetToContentTypeIfUndefined(false))).
